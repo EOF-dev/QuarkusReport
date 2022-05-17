5c9c0cf240cb7bc7993a8609e2ced8c460c0826d
Felipe <felipe@192.168.1.2>
2022-05-17 01:49:54 -0300

    Implementando geração de PDF.

--- src/main/java/br/com/proinddy/resource/HomeResource.java
+++ src/main/java/br/com/proinddy/resource/HomeResource.java
@@ -2,11 +2,16 @@ package br.com.proinddy.resource;
 
 import br.com.proinddy.config.DOA;
 import br.com.proinddy.helper.GenericSQL;
+import net.sf.jasperreports.engine.*;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.*;
 
 @RequestScoped
 @Path("/home")
@@ -26,5 +31,29 @@ public class HomeResource {
         return VERSION;
     }
 
+    @GET
+    @Path("/jasper-report")
+    @Produces( MediaType.APPLICATION_OCTET_STREAM) // studar
+    public Response jasperRepport(String jrxml) {
+        try {
+            //converte pra jasper file
+            JasperReport jasper = JasperCompileManager.compileReport("/home/felipe/JaspersoftWorkspace/MyReports/bin/test.jrxml");
 
+            // retorna relatorio
+            JasperPrint print = JasperFillManager.fillReport(jasper, null, new JREmptyDataSource());
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            // gera pd
+            JasperExportManager.exportReportToPdfStream(print, outputStream);
+
+            Response.ResponseBuilder response = Response.ok(outputStream.toByteArray());
+            response.header("Content-Disposition", "inline;filename=" + "test.pdf");
+            response.header("Content-Type", "application/pdf");
+            return response.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
